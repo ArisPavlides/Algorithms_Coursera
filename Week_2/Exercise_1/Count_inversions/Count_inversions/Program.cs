d6d9a7850f1d9354da2da2b56967843f00206456
Aris Pavlides <arispavlides86@gmail.com>
2018-08-05 17:38:01 +0300

    Added correct week 2 compulsory

--- Week_2/Exercise_1/Count_inversions/Count_inversions/Program.cs
+++ Week_2/Exercise_1/Count_inversions/Count_inversions/Program.cs
@@ -9,12 +9,19 @@ namespace Count_inversions
 {
     class Program
     {
+        public static long num_inv = 0;
+
         static void Main()
         {
             string[] int_arr = File.ReadAllLines(@"C:\Users\arisp\Documents\Algorithms\Week_2\Exercise_1\Integer_array.txt");
             List<int> int_list = int_arr.Select(Int32.Parse).ToList();
 
+            //List<int> int_list = new List<int>() { 6, 5, 4, 3, 2, 1};
             List<int> sorted_list = Split_Lists(int_list);
+
+            Console.WriteLine(num_inv);
+            Console.ReadLine();
+
             foreach (int a in sorted_list)
             {
                 Console.WriteLine(a);
@@ -25,19 +32,24 @@ namespace Count_inversions
         static List<int> Split_Lists(List<int> num_array)
         {
             if (num_array.Count == 1) { return num_array; }
+
             else
             {
                 int num_elements = num_array.Count;
                 int split = num_elements / 2;
+
                 List<int> left = new List<int>();
                 List<int> right = new List<int>();
+
                 for (int i = 0; i < num_elements; i++)
                 {
                     if (i < split) { left.Add(num_array[i]); }
                     else { right.Add(num_array[i]); }
                 };
+
                 List<int> list1 = Split_Lists(left);
                 List<int> list2 = Split_Lists(right);
+
                 return Merge_Lists(list1, list2);
             }
         }
@@ -45,6 +57,7 @@ namespace Count_inversions
         static List<int> Merge_Lists(List<int> list1, List<int> list2)
         {
             List<int> merged_list = new List<int>();
+
             while (list1.Count > 0 || list2.Count > 0)
             {
                 if (list1.Count > 0 && list2.Count > 0)
@@ -54,10 +67,12 @@ namespace Count_inversions
                         merged_list.Add(list1[0]);
                         list1.RemoveAt(0);
                     }
+
                     else
                     {
                         merged_list.Add(list2[0]);
                         list2.RemoveAt(0);
+                        num_inv = num_inv + list1.Count();
                     }
                 }
                 else if (list2.Count == 0)
